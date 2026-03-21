bbbc197e7248474892c3f39148654a4ecf9bac44
puneethprakash <punit.pi@gmail.com>
2026-03-21 11:07:47 +0100

    chore: remove old Express boilerplate files

--- routes/days.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const daysController = require('./../controllers/daysController')
-
-/* GET home page. */
-router.post('/getYearWeekends', daysController.ProcessDates)
-
-module.exports = router
